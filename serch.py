46eb4b67a135845823caceaa2410c7638a4060bc
Shima <bv17037@shibaura-it.ac.jp>
2018-10-03 18:22:02 +0900

    second commit

--- serch.py
+++ serch.py
@@ -2,13 +2,16 @@ import requests
 import xmltodict
 
 def coordinate():
-    full_adress = input("Prease enter the adress:")
-    url = 'http://www.geocoding.jp/api/'
-    payload = {'q':full_adress}
-    result = requests.get(url, payload)
-    print(result.text)
-    resultdict = xmltodict.parse(result.text)
-    print("緯度",resultdict["result"]["coordinate"]["lat"])
-    print("経度",resultdict["result"]["coordinate"]["lng"])
+        try:
+            full_address = input("住所を入力してください:")
+            url = 'http://www.geocoding.jp/api/'
+            payload = {'q':full_address}
+            result = requests.get(url, payload)
+            print(result.text)
+            resultdict = xmltodict.parse(result.text)
+            print("緯度",resultdict["result"]["coordinate"]["lat"])
+            print("経度",resultdict["result"]["coordinate"]["lng"])
+        except Exception as others:
+            print("存在しない住所です")
 
 coordinate()
